5c7c6c20e6506f6c544e570af73ae6d505ddc487
Vaughan Jones <vlj7491@gmail.com>
2021-12-01 16:36:38 +1100

    clean

--- 1_part1.rb
+++ 1_part1.rb
@@ -3,13 +3,9 @@ input = File.read('input/1').split.map(&:to_i)
 count = 0
 previous = input.first
 
-# For each line with an index
 for line in input do
   if line > previous
-    puts "#{line} is greater than #{previous}"
     count += 1
-  else
-    puts "#{line} is not greater than #{previous}"
   end
 
   previous = line
