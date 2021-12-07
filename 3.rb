cc51429241a85c9fdfa7d366ae2dfe65c1bc8395
Vaughan Jones <vlj7491@gmail.com>
2021-12-07 14:04:09 +1100

    clean

--- 3.rb
+++ 3.rb
@@ -1,2 +1,2 @@
 g = File.read('input/3').split.map(&:chars).transpose.map { |a| b = a.select.count { |c| c == "0" }; b > a.size / 2 ? "0" : "1" }.join.to_i(2)
-puts g * (g ^ (2**12 -1))
+puts g * (g ^ (2**12-1))
